375adf4870df4e0ac36223919e38a05f82924ae3
Adam Moss <adam@space48.com>
2016-05-06 19:37:09 +0100

    Testing controller

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -6,8 +6,6 @@ class Index extends \Magento\Framework\App\Action\Action
 {
     public function execute()
     {
-        $this->_view->loadLayout();
-        $this->_view->getLayout()->initMessages();
-        $this->_view->renderLayout();
+        echo "TEST";
     }
 }
